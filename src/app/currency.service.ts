b9c103465100a2345780328a27ce3acdff0195da
Pojerman <45513393+Pojerman@users.noreply.github.com>
2023-06-28 22:08:22 +0300

    changes for stackblitz

--- src/app/currency.service.ts
+++ src/app/currency.service.ts
@@ -1,31 +1,40 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {map} from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyService {
-  private APIKEY = 'zAOf2pI1Cf6Xh4DxYQCO4Vf6FqyvKWec';
-  private API = 'https://api.apilayer.com/currency_data'
-  private LIVE = '/live'
-  private CONVERTER = '/convert'
-  private SOURCE = 'RUB'
-  constructor(private httpClient: HttpClient) {
-  }
+  private APIKEY = 'ADfIeWnWCuhEA7H4DMi1Ls8gmC4IDK1e';
+  private API = 'https://api.apilayer.com/currency_data';
+  private LIVE = '/live';
+  private CONVERTER = '/convert';
+  private SOURCE = 'RUB';
+  constructor(private httpClient: HttpClient) {}
 
   getCurrencyValue(data: any) {
-    return this.httpClient.get(this.API + this.LIVE+`?source=${this.SOURCE}&currencies=${data.join(',')}`, {
-      headers: {
-        "apikey": this.APIKEY
+    return this.httpClient.get(
+      this.API +
+        this.LIVE +
+        `?source=${this.SOURCE}&currencies=${data.join(',')}`,
+      {
+        headers: {
+          apikey: this.APIKEY,
+        },
       }
-    })
+    );
   }
 
-  getConvertAmount(amount:number, currency: string) {
-     return this.httpClient.get(this.API + this.CONVERTER + `?to=${currency}&from=${this.SOURCE}&amount=${amount}`, {
+  getConvertAmount(amount: number, currency: string) {
+    return this.httpClient.get(
+      this.API +
+        this.CONVERTER +
+        `?to=${currency}&from=${this.SOURCE}&amount=${amount}`,
+      {
         headers: {
-          "apikey": this.APIKEY
-        }
-     })
+          apikey: this.APIKEY,
+        },
+      }
+    );
   }
 }
